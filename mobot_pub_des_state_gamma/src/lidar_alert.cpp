4e8ae0353908b2109a076f491ca0a6709d0c9b06
TomShkurti <tomshkurti.school@gmail.com>
2016-03-01 19:24:55 -0500

    Fixed LIDAR.

--- mobot_pub_des_state_gamma/src/lidar_alert.cpp
+++ mobot_pub_des_state_gamma/src/lidar_alert.cpp
@@ -26,6 +26,7 @@ std_srvs::Trigger srv_rst_;
 
 
 void laserCallback(const sensor_msgs::LaserScan& laser_scan){
+    ROS_INFO("CB: %i - %i", ind_min_, ind_max_);
 	if (pindex_<0){
         //for first message received, set up the desired index of LIDAR range to eval
         angle_min_ = laser_scan.angle_min;
@@ -40,13 +41,16 @@ void laserCallback(const sensor_msgs::LaserScan& laser_scan){
         double halfway = angle_min_ + angle_max_ / 2.0;
         double ang_upper_bound = halfway - ONE_SIDE_FANOUT;
         double ang_lower_bound = halfway + ONE_SIDE_FANOUT;
-        int ind_min_ = (int) ((ang_upper_bound - angle_min_)/angle_inc_);
-        int ind_max_ = (int) ((ang_lower_bound - angle_min_)/angle_inc_);
+        ind_min_ = (int) ((ang_upper_bound - angle_min_)/angle_inc_);
+        ind_max_ = (int) ((ang_lower_bound - angle_min_)/angle_inc_);
         ROS_INFO("LIDAR setup: ping_index = %d, bound indices (%d, %d)",pindex_, ind_min_, ind_max_);
     }
 
+    ROS_INFO("CB: %i - %i", ind_min_, ind_max_);
+
     bool obstruction = false;
     for(int i = ind_min_; i <= ind_max_; i++){
+        //ROS_INFO("  %d: %f",i, laser_scan.ranges[i]);
     	if(laser_scan.ranges[i] < LIDAR_SAFE_DIST){
     		obstruction = true;
     		ROS_WARN("I AM SO TRIGGERED RIGHT NOW...");
