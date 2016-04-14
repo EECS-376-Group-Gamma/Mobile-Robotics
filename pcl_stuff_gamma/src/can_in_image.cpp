837f1e4ef63f93882bc06ed4268807e12c151533
TomShkurti <tomeshkurti.school@gmail.com>
2016-04-13 21:09:52 -0400

    Final!

--- pcl_stuff_gamma/src/can_in_image.cpp
+++ pcl_stuff_gamma/src/can_in_image.cpp
@@ -12,12 +12,19 @@ int main(int argc, char** argv) {
 	ros::init(argc, argv, "yes_we_can");
 	ros::NodeHandle nh;
 
+	ROS_INFO("Initted.");
+
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr pclKinect_clr_ptr(new pcl::PointCloud<pcl::PointXYZRGB>);
+
+	ROS_INFO("DONE");
 
 	if(pcl::io::loadPCDFile<pcl::PointXYZRGB> ("/home/tes77/ros_ws/src/learning_ros/Part_3/pcd_images", *pclKinect_clr_ptr) == -1){
 		ROS_ERROR("ERRORRRRRRR...");
 		return 1;
 	}
+
+	ROS_INFO("Double done.");
+
 	pclKinect_clr_ptr->header.frame_id = "camera_depth_optical_frame";
 
 
